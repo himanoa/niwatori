e9e13e78f847f2626b1ac960dba7665de2493e10
himanoa <matsunoappy@gmail.com>
2017-02-15 11:17:55 +0900

    :clean:

--- app/src/renderer/components/Tweet.vue
+++ app/src/renderer/components/Tweet.vue
@@ -2,34 +2,26 @@
   <div class="tweet">
     <input type="hidden" :value="selectedTweet">
     <input type="hidden" :value="index">
-    <el-row v-if="tweet['retweeted_status']" :gutter="10">
+    <el-row :gutter="10">
       <el-col :span="3">
-        <img :src="tweet['retweeted_status']['user']['profile_image_url_https']" style="width:100%;">
+        <img v-if="tweet['retweeted_status']" :src="tweet['retweeted_status']['user']['profile_image_url_https']" style="color: green; width:100%;">
+        <img v-else :src="tweet['user']['profile_image_url_https']" style="width:100%;">
       </el-col>
       <el-col :span="21">
-        <el-row>
+        <el-row v-if="tweet['retweeted_status']" style="color: green;">
           <strong>{{tweet['retweeted_status']['user']['name']}}</strong>
           <span>@{{tweet['retweeted_status']['user']['screen_name']}}</span>
         </el-row>
-        <el-row>
-          <p style="width: 100%;">{{tweet['retweeted_status']['text']}}</p>
-        </el-row>
-        <el-row>
-          <p>retweeted by <img style="height: 30px;":src="tweet['user']['profile_image_url_https']">@{{tweet['user']['screen_name']}}</p>
-        </el-row>
-      </el-col>
-    </el-row>
-    <el-row v-else :gutter="10">
-      <el-col :span="3">
-        <img :src="tweet['user']['profile_image_url_https']" style="width:100%;">
-      </el-col>
-      <el-col :span="21">
-        <el-row>
+        <el-row v-else>
           <strong>{{tweet['user']['name']}}</strong>
           <span>@{{tweet['user']['screen_name']}}</span>
         </el-row>
         <el-row>
-          <p style="width: 100%;">{{tweet['text']}}</p>
+          <p v-if="tweet['retweeted_status']"style="width: 100%; color:green;">{{tweet['retweeted_status']['text']}}</p>
+          <p v-else style="width: 100%;">{{tweet['text']}}</p>
+        </el-row>
+        <el-row v-if="tweet['retweeted_status']">
+          <p style="color: green;">retweeted by <img style="height: 30px;":src="tweet['user']['profile_image_url_https']">@{{tweet['user']['screen_name']}}</p>
         </el-row>
       </el-col>
     </el-row>
