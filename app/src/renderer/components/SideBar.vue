2f10c465f7bd51204cc252ad38b974b92dce5e25
himanoa <matsunoappy@gmail.com>
2017-03-07 14:58:43 +0900

    activate active-link color change blue

--- app/src/renderer/components/SideBar.vue
+++ app/src/renderer/components/SideBar.vue
@@ -1,10 +1,10 @@
 <template>
-  <el-menu class="el-menu-vertical-demo" theme="dark">
+  <el-menu default-active="1-1" class="el-menu-vertical-demo" theme="dark">
     <el-submenu index="1">
       <template slot="title">アカウント</template>
-      <el-menu-item-group v-for="id in clientIds" :title="accounts[id].profile.screen_name">
-        <router-link tag="li" class='el-menu-item' :to="{ name: 'timeline', params: { accountIdStr: id } }">Home</router-link>
-        <router-link tag="li" class='el-menu-item' :to="{ name: 'mention', params: { accountIdStr: id } }">Mention</router-link>
+      <el-menu-item-group v-for="(id, index) in clientIds" :title="accounts[id].profile.screen_name">
+        <router-link tag="li" :index="'1-' + (index+1)" class='el-menu-item' :to="{ name: 'timeline', params: { accountIdStr: id } }">Home</router-link>
+        <router-link tag="li" :index="'1-' + (index+2)" class='el-menu-item' :to="{ name: 'mention', params: { accountIdStr: id } }">Mention</router-link>
       </el-menu-item-group>
     </el-submenu>
     <el-menu-item id="add-account" index="2"><a href="#" @click="clickedAddAccount"><i class="el-icon-plus"></i>アカウント追加</a></el-menu-item>
@@ -35,5 +35,8 @@ export default {
 a {
   text-decoration: none;
   color: #bfcbd9;
+}
+.router-link-active {
+  color: #1387FF;
 }
 </style>
