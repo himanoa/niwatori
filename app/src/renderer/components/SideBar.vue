6a749157c83c69d9ee0215d2f089f32baa823480
himanoa <matsunoappy@gmail.com>
2017-03-07 19:47:58 +0900

    change css

--- app/src/renderer/components/SideBar.vue
+++ app/src/renderer/components/SideBar.vue
@@ -1,16 +1,13 @@
 <template>
   <el-menu default-active="1-1" class="el-menu-vertical-demo" theme="dark">
-    <el-submenu index="1">
-      <template slot="title">アカウント</template>
-      <el-menu-item-group v-for="(id, index) in clientIds" :title="accounts[id].profile.screen_name">
-        <router-link tag="li" :index="'1-' + (index+1)" class='el-menu-item' :to="{ name: 'timeline', params: { accountIdStr: id } }">Home</router-link>
-        <router-link tag="li" :index="'1-' + (index+2)" class='el-menu-item' :to="{ name: 'mention', params: { accountIdStr: id } }">Mention</router-link>
-        <el-submenu :index="'1-' + (index+3)" >
-          <template slot="title">リスト</template>
-          <router-link v-for="list in lists(id)" tag="li" class='el-menu-item' :to="{ name: 'list', params: { accountIdStr: id, listId: list.id_str } }">{{ list.name }}</router-link>
-        </el-submenu>
-      </el-menu-item-group>
-    </el-submenu>
+    <el-menu-item-group v-for="(id, index) in clientIds" :title="accounts[id].profile.screen_name">
+      <router-link tag="li" :index="'1-' + (index+1)" class='el-menu-item' :to="{ name: 'timeline', params: { accountIdStr: id } }">Home</router-link>
+      <router-link tag="li" :index="'1-' + (index+2)" class='el-menu-item' :to="{ name: 'mention', params: { accountIdStr: id } }">Mention</router-link>
+      <el-submenu :index="'1-' + (index+3)" >
+        <template slot="title">リスト</template>
+        <router-link v-for="list in lists(id)" tag="li" class='el-menu-item child' :to="{ name: 'list', params: { accountIdStr: id, listId: list.id_str } }">{{ list.name }}</router-link>
+      </el-submenu>
+    </el-menu-item-group>
     <el-menu-item id="add-account" index="2"><a href="#" @click="clickedAddAccount"><i class="el-icon-plus"></i>アカウント追加</a></el-menu-item>
   </el-menu>
 </template>
@@ -28,13 +25,36 @@ export default {
   }
 }
 </script>
+<style>
+.el-submenu__title {
+  line-height: 30px;
+  height: 30px;
+}
+.el-menu-item-group__title {
+  color: #13CE66;
+  font-weight: bolder;
+  padding-top: 7.5px;
+  padding-bottom: 7.5px;
+}
+</style>
 <style scoped>
+.child {
+  padding-left: 20px !important;
+}
 #add-account .is-active {
   color: #d1dbe5;
 }
 .el-menu {
   overflow-y: scroll;
   height: 100%;
+}
+.el-menu-item {
+  height: 30px;
+  line-height: 30px;
+}
+.el-menu-submenu__title {
+  height: 30px;
+  line-height: 30px;
 }
 a {
   text-decoration: none;
